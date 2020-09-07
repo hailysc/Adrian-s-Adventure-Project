ce60913d48599e4d933c82a42f62f5c3e5bf4161
Haily San <hailysanc04@gmail.com>
2020-09-07 13:56:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function showHideOne() {
+  var showTextOne = document.getElementById("textOne");
+  showTextOne.classList.toggle('hiddenOne'); 
+}
+function showHideTwo() {
+  var showTextTwo = document.getElementById("textTwo");
+  showTextTwo.classList.toggle('hiddenTwo'); 
+}
+
+function showHideThree() {
+  var showTextThree = document.getElementById("textThree");
+  showTextThree.classList.toggle('hiddenThree'); 
+}
+
+function showHideFour() {
+  var showTextFour = document.getElementById("textFour");
+  showTextFour.classList.toggle('hiddenFour'); 
+}
+
+
